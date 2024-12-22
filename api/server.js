415d72113a8dfb76522ae298a51a7d32b1300f51
San4568GH <sansahu098@gmail.com>
2024-12-23 03:26:39 +0530

    trial

--- api/server.js
+++ api/server.js
@@ -30,7 +30,7 @@ app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 //dirname in es6,__dirname in cj
 
 //MongoDB Connection
-mongoose.connect('process.env.MONGO_URL', {
+mongoose.connect('process.env.MONGO_URI', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 }).then(() => console.log('MongoDB connected successfully'))
